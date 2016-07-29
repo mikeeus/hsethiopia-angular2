a416f98bd9e03e773df148c3b25aadf91e9c1c56
Mikias <itsmikias@gmail.com>
2016-07-29 12:30:04 +0300

    Add imports and exports attributes

--- db/migrate/20160729092824_create_imports.rb
+++ db/migrate/20160729092824_create_imports.rb
@@ -1,7 +1,19 @@
 class CreateImports < ActiveRecord::Migration[5.0]
   def change
     create_table :imports do |t|
-
+      t.belongs_to :hscode, foreign_key: true, index: true
+      t.integer :year,            null: false, index: true
+      t.integer :code,            null: false, index: true
+      t.string  :description,     null: false
+      t.string  :country_origin,  null: false, index: true
+      t.string  :country_consignment, null: false
+      t.decimal :net_mass, precision: 13, scale: 2, 
+        null: false
+      t.decimal :cif_usd, precision: 13, scale: 2, 
+        null: false
+      t.decimal :cif_etb, precision: 13, scale: 2, 
+        null: false
+        
       t.timestamps
     end
   end
--- db/migrate/20160729092850_create_exports.rb
+++ db/migrate/20160729092850_create_exports.rb
@@ -1,7 +1,18 @@
 class CreateExports < ActiveRecord::Migration[5.0]
   def change
     create_table :exports do |t|
-
+      t.belongs_to :hscode, foreign_key: true, index: true
+      t.integer :year,        null: false, index: true
+      t.integer :code,        null: false, index: true
+      t.string  :description, null: false
+      t.string  :destination, null: false, index: true
+      t.decimal :net_mass, precision: 13, scale: 2, 
+        null: false
+      t.decimal :fob_usd, precision: 13, scale: 2, 
+        null: false
+      t.decimal :fob_etb, precision: 13, scale: 2, 
+        null: false
+        
       t.timestamps
     end
   end
