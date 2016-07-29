8f5946e1c9925bbea35fb74ff76a42a0079b6de7
Mikias <itsmikias@gmail.com>
2016-07-30 00:37:53 +0300

    Add refresh matviews route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get '/country/:country' => 'matviews#country', as: :country
   get '/year/:year' => 'matviews#year', as: :year
   get '/hscode_matview/:code' => 'matviews#hscode', as: :hscode_matview
-
+  get '/refresh_matviews' => 'matviews#refresh_matviews', as: :refresh_matviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
