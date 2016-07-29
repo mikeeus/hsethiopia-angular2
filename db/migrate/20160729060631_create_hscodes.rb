94de3c609d1d260baf016300d6e1a52280275426
Mikias <itsmikias@gmail.com>
2016-07-29 10:16:20 +0300

    Create database with sql schema format

--- db/migrate/20160729060631_create_hscodes.rb
+++ db/migrate/20160729060631_create_hscodes.rb
@@ -1,7 +1,18 @@
 class CreateHscodes < ActiveRecord::Migration[5.0]
   def change
     create_table :hscodes do |t|
-
+      t.integer :code,        null: false, index:true
+      t.string  :description, null: false
+      t.string  :unit,        null: false, default: "UN"
+      t.string  :special_permission, null: false, default: "None"
+      t.integer :duty,     null: false, default: 0
+      t.integer :excise,   null: false, default: 0
+      t.integer :vat,      null: false, default: 0
+      t.integer :sur,      null: false, default: 0
+      t.integer :withhold, null: false, default: 0
+      t.integer :ss1,      null: false, default: 0
+      t.integer :ss2,      null: false, default: 0
+      
       t.timestamps
     end
   end
