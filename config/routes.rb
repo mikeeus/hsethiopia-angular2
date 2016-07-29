7ccbdea564758ed9995eac974bbbe7a7fec7db42
Mikias <itsmikias@gmail.com>
2016-07-29 22:44:41 +0300

    Add matview routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   resources :exports, only: [:index, :show]
   resources :imports, only: [:index, :show]
   resources :hscodes, only: [:index, :show]
+
+  get '/country/:country' => 'matviews#country', as: :country
+  get '/year/:year' => 'matviews#year', as: :year
+  get '/hscode_matview/:code' => 'matviews#hscode_imports', as: :hscode_matview
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
