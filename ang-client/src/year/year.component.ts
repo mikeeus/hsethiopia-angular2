5f641e8d3e940a333d85a06f2b52c0c2db8e3cb0
Mikias <itsmikias@gmail.com>
2016-07-30 14:32:20 +0300

    Add Input

--- ang-client/src/year/year.component.ts
+++ ang-client/src/year/year.component.ts
@@ -1,8 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'year',
   templateUrl: '/year/year.component.html',
 })
-export class YearComponent {
+export class YearComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
 }
