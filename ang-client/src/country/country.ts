8d42e2176674a0197950723ac42f1a72434cd44e
Mikias <itsmikias@gmail.com>
2016-07-30 16:08:18 +0300

    Add country model

---
+++ ang-client/src/country/country.ts
@@ -0,0 +1,7 @@
+import {CountryImports} from './country-imports';
+import {CountryExports} from './country-exports';
+
+export class Country {
+  countryImports: CountryImports;
+  countryExports: CountryExports;
+}
