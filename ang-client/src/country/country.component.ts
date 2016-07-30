5c48d833c865376573501ef3b01467fa5043f041
Mikias <itsmikias@gmail.com>
2016-07-30 16:22:14 +0300

    Add OnInit and model data to country component

--- ang-client/src/country/country.component.ts
+++ ang-client/src/country/country.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
 import {Country} from './country';
+import {CountryImports} from './country-imports';
+import {CountryExports} from './country-exports';
 import {CountryService} from './country.service';
 
 @Component({
@@ -11,6 +13,26 @@ import {CountryService} from './country.service';
     CountryService
   ]
 })
-export class CountryComponent {
-  
+export class CountryComponent implements OnInit {
+  country: string;
+  countryImports: CountryImports;
+  countryExports: CountryExports;
+  sub: any;
+  constructor(
+    private countryService: CountryService,
+    private route: Router
+  ) {}
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      if (params['country'] !== undefined) {
+        this.country = params['country'];
+        this.countryService.getCountryData(this.country)
+            .then(countryData => {
+              this.countryImports = countryData.countryImports;
+              this.countryExports = countryData.countryExports
+            });
+      }
+    });
+  }
 }
