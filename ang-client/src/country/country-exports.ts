fe85a79a624ebfe3caf46e8a01512d2f30addb9e
Mikias <itsmikias@gmail.com>
2016-07-30 16:12:57 +0300

    Add country related models

---
+++ ang-client/src/country/country-exports.ts
@@ -0,0 +1,7 @@
+export class CountryExports {
+  country: string;
+  year: number;
+  fob_usd: number;
+  fob_etb: number;
+  net_mass: number;
+}
---
+++ ang-client/src/country/country-imports.ts
@@ -0,0 +1,7 @@
+export class CountryImports {
+  country: string;
+  year: number;
+  cif_usd: number;
+  cif_etb: number;
+  net_mass: number;
+}
