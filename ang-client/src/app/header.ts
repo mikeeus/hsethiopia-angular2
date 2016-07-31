9bd0ba5e08b7184c1bec8ce53ed3d276a35ebeca
Mikias <itsmikias@gmail.com>
2016-07-31 14:44:09 +0300

    Add @angular/forms, edit header links

--- ang-client/src/app/header.ts
+++ ang-client/src/app/header.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
+import {CORE_DIRECTIVES} from '@angular/common';
+import {DROPDOWN_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'Header',
   template: require('./header.html'),
+  directives: [
+    DROPDOWN_DIRECTIVES
+  ],
   styleUrls: [
     './app/header.scss'
   ]
