98ee3ffae72c1cbc252043ed7d0c6f37a400a2be
Mikias <itsmikias@gmail.com>
2016-07-31 14:48:59 +0300

    Add dropdown functions

--- ang-client/src/app/header.ts
+++ ang-client/src/app/header.ts
@@ -15,6 +15,22 @@ import {DROPDOWN_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 })
 export class Header {
   constructor(private router: Router) {}
+  public disabled:boolean = false;
+  public status:{isopen:boolean} = {isopen: false};
+  public items:Array<string> = ['The first choice!',
+  'Choice 2', 'And a third!'
+  ];
+
+  public toggled(open:boolean):void {
+    console.log('Drop is now: ', open);
+  }
+
+  public toggleDropdown($event:MouseEvent):void{
+    $event.preventDefault();
+    $event.stopPropagation();
+    this.status.isopen = !this.status.isopen;
+  }
+
 
   gotoLink(path: string) {
     let link = [path];
