1782dde4e80109c2f3281b899d22d69174af3b68
Mikias <itsmikias@gmail.com>
2016-07-31 15:33:12 +0300

    Add dropdown links to navbar

--- ang-client/src/app/header.ts
+++ ang-client/src/app/header.ts
@@ -20,6 +20,12 @@ export class Header {
   public items:Array<string> = ['The first choice!',
   'Choice 2', 'And a third!'
   ];
+  public countries:Array<string> = ['Germany',
+  'China', 'United States'
+  ];
+  public years:Array<number> = [
+    2016, 2015, 2014, 2013, 2012, 2011
+  ];
 
   public toggled(open:boolean):void {
     console.log('Drop is now: ', open);
@@ -31,9 +37,18 @@ export class Header {
     this.status.isopen = !this.status.isopen;
   }
 
-
   gotoLink(path: string) {
     let link = [path];
+    this.router.navigate(link);
+  }
+
+  gotoYear(year: number) {
+    let link = ['year/', year];
+    this.router.navigate(link);
+  }
+
+  gotoCountry(country: string) {
+    let link = ['country/', country];
     this.router.navigate(link);
   }
 }
