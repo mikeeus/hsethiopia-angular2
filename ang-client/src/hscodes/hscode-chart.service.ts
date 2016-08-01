28969fe9227fdb411e016f218924fe7541ab97df
Mikias <itsmikias@gmail.com>
2016-08-01 11:09:48 +0300

    Add hscode service

---
+++ ang-client/src/hscodes/hscode-chart.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+import {HscodeChart} from '../models/hscode-chart';
+
+
+@Injectable()
+
+export class HscodeChartService {
+  constructor(private http: Http) {}
+// events
+
+  getChartData(hscode: string) {
+    return this.http.get('http://localhost:3000/charts/hscode/' + hscode)
+      .toPromise()
+      .then(response => response.json() as HscodeChart)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any) {
+    console.error('An error occurred', error);
+
+    return Promise.reject(error.message || error);
+  }
+}
