7cd9473ab9ac35c15f0ac8f0bd7bb149b6eb5bfc
Mikias <itsmikias@gmail.com>
2016-08-01 08:31:34 +0300

    Update charts routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   resources :imports, only: [:index, :show]
   resources :hscodes, only: [:index, :show]
 
-  get '/country/:country' => 'matviews#country', as: :country
-  get '/year/:year' => 'matviews#year', as: :year
-  get '/hscode_matview/:code' => 'matviews#hscode', as: :hscode_matview
+  # Charts/ Matviews routes
+  get '/charts/country/:country' => 'matviews#country', as: :country_chart
+  get '/charts/year/:year' => 'matviews#year', as: :year
+  get '/charts/hscode/:code' => 'matviews#hscode', as: :hscode_matview
   get '/refresh_matviews' => 'matviews#refresh_matviews', as: :refresh_matviews
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
