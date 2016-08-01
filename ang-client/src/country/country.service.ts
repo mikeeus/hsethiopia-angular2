e622e9b94ec8afea7b7a51eb6fd24d27adc75536
Mikias <itsmikias@gmail.com>
2016-08-01 08:12:02 +0300

    Add getCountryChartData, refactor urls

--- ang-client/src/country/country.service.ts
+++ ang-client/src/country/country.service.ts
@@ -3,16 +3,26 @@ import {Http} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 import {Country} from './country';
+import {CountryChart} from './country-chart';
+
 
 @Injectable()
 export class CountryService {
   countryUrl: 'http://localhost:3000/country/';
+  countryChartUrl: 'http://localhost:3000/charts/country/';
   constructor(private http: Http) {}
 
   getCountryData(country: string) {
-    return this.http.get('http://localhost:3000/country/' + country)
+    return this.http.get(this.countryUrl + country)
       .toPromise()
       .then(response => response.json() as Country)
+      .catch(this.handleError);
+  }
+
+  getCountryChartData(country: string) {
+    return this.http.get(this.countryChartUrl + country)
+      .toPromise()
+      .then(response => response.json() as CountryChart)
       .catch(this.handleError);
   }
 
