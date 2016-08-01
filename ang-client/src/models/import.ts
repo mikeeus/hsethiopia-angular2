b019a552d38eb8dd591554ae0e03430ba62c6283
Mikias <itsmikias@gmail.com>
2016-08-01 08:15:18 +0300

    Add Import model

---
+++ ang-client/src/models/import.ts
@@ -0,0 +1,9 @@
+export class Import {
+  code: string;
+  description: string;
+  country_origin: string;
+  country_consignment: string;
+  cif_usd: number;
+  cif_etb: number;
+  net_mass: number;
+}
