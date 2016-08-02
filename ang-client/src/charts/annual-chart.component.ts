196acdea4529faca3fd0d2e7b1a0d7b6736a4c5f
Mikias <itsmikias@gmail.com>
2016-08-02 10:10:05 +0300

    refactor chart options

--- ang-client/src/charts/annual-chart.component.ts
+++ ang-client/src/charts/annual-chart.component.ts
@@ -9,6 +9,7 @@ import {AnnualChartService} from './annual-chart.service';
 import {AnnualChart} from '../models/annual-chart';
 
 import {years} from './years';
+import {chartOptions} from './chart-options';
 
 @Component({
   selector: 'annual-chart',
@@ -29,41 +30,7 @@ export class AnnualChartComponent implements OnInit {
     private route: ActivatedRoute
   ) {}
   sub: any;
-  public barChartOptions: any = {
-    scaleShowVerticalLines: false,
-    responsive: true,
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:true,
-          userCallback: function (value){
-            if (value < 1){
-              return "$" + value.toFixed(1);
-            } else if (value < 10000){
-              return "$" + value;
-            } else if (value < 1000000){
-              return "$" + (value/1000)+",000";
-            } else if (value < 1000000000){
-              return "$" + value/1000000 + " Million";
-            } else {
-              return "$" + value/1000000000 + " Billion";
-            }
-          }
-        }
-      }],
-      xAxes: [{
-        ticks: {
-          userCallback: function(value) {
-
-          if ( value.length > 15 ){
-            return value.slice(0,13) + "...";
-          }
-          return value;
-          }
-        }
-      }]
-    }
-  };
+  public barChartOptions: any = chartOptions;
   public barChartLabels: string[] = years;
   public barChartType: string = 'bar';
   public barChartLegend: boolean = true;
