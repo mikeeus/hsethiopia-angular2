c351bc802a96f32b84b8a110619b7e5ab2391ece
Mikias <itsmikias@gmail.com>
2016-08-02 09:25:34 +0300

    Create annual-chart.service to start refactoring

---
+++ ang-client/src/charts/annual-chart.service.ts
@@ -0,0 +1,56 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+import {AnnualChartData} from '../models/annual-chart-data';
+import {AnnualChart} from '../models/annual-chart';
+import {years} from './years';
+
+@Injectable()
+export class AnnualChartService {
+  constructor(private http: Http) {}
+  countryChartUrl = 'http://localhost:3000/charts/country/';
+  hscodeChartUrl = 'http://localhost:3000/charts/hscode/';
+
+  getCountryChartData(country: string) {
+    return this.http.get(this.countryChartUrl + country)
+      .toPromise()
+      .then(response => {
+        return this.populateChartData(response.json() as AnnualChart);
+        }
+      )
+      .catch(this.handleError);
+  }
+
+  getHscodeChartData(code: number) {
+    return this.http.get(this.hscodeChartUrl + code)
+      .toPromise()
+      .then(response => {
+        return this.populateChartData(response.json() as AnnualChart);
+      })
+      .catch(this.handleError);
+  }
+  
+  populateChartData(chart: AnnualChart) {
+    let labels = years;
+    let _chartData: Array<any> = new Array(2);
+
+    _chartData[0] = {data: new Array(labels.length), label: "Imports"};
+    _chartData[1] = {data: new Array(labels.length), label: "Exports"};
+
+    for (let i = 0; i < labels.length; i++){
+      _chartData[0].data[i] = chart.annualImports[labels[i]];
+    }
+    for (let j = 0; j < labels.length; j++){
+      _chartData[1].data[j] = chart.annualExports[labels[j]];
+    }
+
+    return _chartData;
+  }
+
+  private handleError(error: any) {
+    console.error('An error occurred', error);
+
+    return Promise.reject(error.message || error);
+  }
+}
