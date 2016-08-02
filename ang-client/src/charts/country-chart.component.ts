c40324fc60cb081008d916a37dd6d4d3c90e2105
Mikias <itsmikias@gmail.com>
2016-08-02 08:43:14 +0300

    Add charts options to format numbers

--- ang-client/src/charts/country-chart.component.ts
+++ ang-client/src/charts/country-chart.component.ts
@@ -32,12 +32,36 @@ export class CountryChartComponent implements OnInit {
     scaleShowVerticalLines: false,
     responsive: true,
     scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
+      yAxes: [{
+        ticks: {
+          beginAtZero:true,
+          userCallback: function (value){
+            if (value < 1){
+              return "$" + value.toFixed(1);
+            } else if (value < 10000){
+              return "$" + value;
+            } else if (value < 1000000){
+              return "$" + (value/1000)+",000";
+            } else if (value < 1000000000){
+              return "$" + value/1000000 + " Million";
+            } else {
+              return "$" + value/1000000000 + " Billion";
+            }
+          }
         }
+      }],
+      xAxes: [{
+        ticks: {
+          userCallback: function(value) {
+
+          if ( value.length > 15 ){
+            return value.slice(0,13) + "...";
+          }
+          return value;
+          }
+        }
+      }]
+    }
   };
   public barChartLabels: string[] = years;
   public barChartType: string = 'bar';
