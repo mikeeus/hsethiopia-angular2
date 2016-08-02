4beb29c1544a11a83eba5d352678fdc1dec0e378
Mikias <itsmikias@gmail.com>
2016-08-02 11:53:35 +0300

    Complete populateChartData function with proper models

--- ang-client/src/charts/top-ten-charts.service.ts
+++ ang-client/src/charts/top-ten-charts.service.ts
@@ -27,34 +27,11 @@ export class TopTenChartsService {
       topTenHscodesExport: {data: new Array(10), label: 'Hscodes by Export'},
     };
 
-    for (let x = 0; x < 10; x++){
-      _chartData.topTenCountriesImport.data[x] = chart.topTenCountriesImport[x][0];
-      _chartData.topTenCountriesExport.data[x] = chart.topTenCountriesExport[x][0];
-      _chartData.topTenHscodesImport.data[x] = chart.topTenHscodesImport[x][0];
-      _chartData.topTenHscodesExport.data[x] = chart.topTenHscodesExport[x][0];
-    }
-
-    for (let a = 0; a < 10; a ++) {
-      _chartData[0] = {data: new Array(), label: l};
-    }
-      
-
-
-    _chartData[1] = {data: new Array(length), label: "Countries by Exports"};
-    _chartData[2] = {data: new Array(length), label: "Hscodes by Imports"};
-    _chartData[3] = {data: new Array(length), label: "Hscodes by Exports"};
-
-    for (let i = 0; i < length; i++){
-      _chartData[0].data[i] = chart.topTenCountriesImport[i][0];
-    }
-    for (let j = 0; j < length; j++){
-      _chartData[1].data[j] = chart.topTenCountriesExport[j][0];
-    }
-    for (let k = 0; k < length; k++){
-      _chartData[0].data[k] = chart.topTenHscodesImport[k][0];
-    }
-    for (let l = 0; l < length; l++){
-      _chartData[1].data[l] = chart.topTenHscodesExport[l][0];
+    for (let i = 0; i < 10; i++){
+      _chartData.topTenCountriesImport.data[i] = chart.topTenCountriesImport[i][0];
+      _chartData.topTenCountriesExport.data[i] = chart.topTenCountriesExport[i][0];
+      _chartData.topTenHscodesImport.data[i] = chart.topTenHscodesImport[i][0];
+      _chartData.topTenHscodesExport.data[i] = chart.topTenHscodesExport[i][0];
     }
 
     return _chartData;
