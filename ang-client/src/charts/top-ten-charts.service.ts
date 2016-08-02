d0172849b33038b0321a88fc33007896deb55738
Mikias <itsmikias@gmail.com>
2016-08-02 11:50:01 +0300

    Add populateChartData function outline

--- ang-client/src/charts/top-ten-charts.service.ts
+++ ang-client/src/charts/top-ten-charts.service.ts
@@ -18,22 +18,47 @@ export class TopTenChartsService {
       .catch(this.handleError);
   }
 
-  // populateChartData(chart: AnnualChart) {
-  //   let labels = years;
-  //   let _chartData: Array<any> = new Array(2);
+  populateChartData(chart: TopTenCharts) {
+    let length = 10;
+    let _chartData: TopTenCharts = {
+      topTenCountriesImport: {data: new Array(10), label: 'Countries by Import'},
+      topTenCountriesExport: {data: new Array(10), label: 'Countries by Export'},
+      topTenHscodesImport: {data: new Array(10), label: 'Hscodes by Import'},
+      topTenHscodesExport: {data: new Array(10), label: 'Hscodes by Export'},
+    };
 
-  //   _chartData[0] = {data: new Array(labels.length), label: "Imports"};
-  //   _chartData[1] = {data: new Array(labels.length), label: "Exports"};
+    for (let x = 0; x < 10; x++){
+      _chartData.topTenCountriesImport.data[x] = chart.topTenCountriesImport[x][0];
+      _chartData.topTenCountriesExport.data[x] = chart.topTenCountriesExport[x][0];
+      _chartData.topTenHscodesImport.data[x] = chart.topTenHscodesImport[x][0];
+      _chartData.topTenHscodesExport.data[x] = chart.topTenHscodesExport[x][0];
+    }
 
-  //   for (let i = 0; i < labels.length; i++){
-  //     _chartData[0].data[i] = chart.annualImports[labels[i]];
-  //   }
-  //   for (let j = 0; j < labels.length; j++){
-  //     _chartData[1].data[j] = chart.annualExports[labels[j]];
-  //   }
+    for (let a = 0; a < 10; a ++) {
+      _chartData[0] = {data: new Array(), label: l};
+    }
+      
 
-  //   return _chartData;
-  // }
+
+    _chartData[1] = {data: new Array(length), label: "Countries by Exports"};
+    _chartData[2] = {data: new Array(length), label: "Hscodes by Imports"};
+    _chartData[3] = {data: new Array(length), label: "Hscodes by Exports"};
+
+    for (let i = 0; i < length; i++){
+      _chartData[0].data[i] = chart.topTenCountriesImport[i][0];
+    }
+    for (let j = 0; j < length; j++){
+      _chartData[1].data[j] = chart.topTenCountriesExport[j][0];
+    }
+    for (let k = 0; k < length; k++){
+      _chartData[0].data[k] = chart.topTenHscodesImport[k][0];
+    }
+    for (let l = 0; l < length; l++){
+      _chartData[1].data[l] = chart.topTenHscodesExport[l][0];
+    }
+
+    return _chartData;
+  }
 
   private handleError(error: any) {
     console.error('An error occurred', error);
