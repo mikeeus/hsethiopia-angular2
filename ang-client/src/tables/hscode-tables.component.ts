221a423637d46ab991290aadb79c4add4fc444b8
Mikias <itsmikias@gmail.com>
2016-08-03 11:26:55 +0300

    Create hscode tables component

---
+++ ang-client/src/tables/hscode-tables.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+import {Hscode} from '../models/hscode';
+
+@Component({
+  selector: 'hscode-tables',
+  templateUrl: './hscode-table.component.html'
+})
+export class HscodeTablesComponent implements OnInit {
+
+  ngOnInit() {
+    
+  }
+}
