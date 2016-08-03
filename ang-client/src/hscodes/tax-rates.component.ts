8ea10998b19d1933d80abd19b5d8f47eba87cc4c
Mikias <itsmikias@gmail.com>
2016-08-03 08:30:13 +0300

    Add tax-rates styles

--- ang-client/src/hscodes/tax-rates.component.ts
+++ ang-client/src/hscodes/tax-rates.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'tax-rates',
-  templateUrl: './tax-rates.component.html'
+  templateUrl: './tax-rates.component.html',
+  styleUrls: ['./tax-rates.scss']
 })
 export class TaxRatesComponent {
 
