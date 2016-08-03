7faf6e42d73aae172983bae65dc94c274b2e7714
Mikias <itsmikias@gmail.com>
2016-08-03 07:54:58 +0300

    Comment out function logs

--- ang-client/src/charts/annual-chart.component.ts
+++ ang-client/src/charts/annual-chart.component.ts
@@ -40,10 +40,10 @@ export class AnnualChartComponent implements OnInit {
   ];
 
   public chartClicked(e: any):void {
-    console.log(e);
+    // console.log(e);
   }
   public chartHovered(e: any): void {
-    console.log(e);
+    // console.log(e);
   }
 
   ngOnInit() {
