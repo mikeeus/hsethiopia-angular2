af313db4a9d535d32346efe4b0ee7f365493c193
Mikias <itsmikias@gmail.com>
2016-08-03 09:29:58 +0300

    Add related codes method

--- app/models/hscode.rb
+++ app/models/hscode.rb
@@ -1,4 +1,11 @@
 class Hscode < ApplicationRecord
   has_many :imports
   has_many :exports
+
+  def related_codes
+    bot_range = (self.code/10000)*10000
+    top_range = bot_range + 9999
+    related_codes = Hscode.where(code: bot_range..top_range)
+    return related_codes
+  end
 end
