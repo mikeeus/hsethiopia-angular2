53963757a66fd1d5f8134667c2cb14c5d0e028bf
Mikias <itsmikias@gmail.com>
2016-08-05 16:21:40 +0300

    Remove hscodes list and service

--- ang-client/src/hscodes/hscodes.component.ts
+++ ang-client/src/hscodes/hscodes.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
 import {Hscode} from '../models/hscode';
-import {HscodeService} from './hscode.service';
+// import {HscodeService} from './hscode.service';
 
 @Component({
   selector: 'hscodes',
@@ -10,26 +10,18 @@ import {HscodeService} from './hscode.service';
   directives: [
   ],
   providers: [
-    HscodeService
+
   ]
 })
 export class HscodesComponent implements OnInit {
   hscodes: Hscode[];
   error: any;
   constructor(
-    private hscodeService: HscodeService,
     private router: Router
   ) {}
 
-  getHscodes() {
-    this.hscodeService
-        .getHscodes()
-        .then(hscodes => this.hscodes = hscodes)
-        .catch(error => this.error = error);
-  }
-
   ngOnInit(){
-    this.getHscodes();
+    
   }
 
   gotoDetail(hscode: Hscode) {
