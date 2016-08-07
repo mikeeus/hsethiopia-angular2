beb3a2e26d7b7e014133ed7b455fdd3ac4129b5d
Mikias <itsmikias@gmail.com>
2016-08-07 17:21:09 +0300

    Adjust search method to account for multiple words

--- app/models/hscode_search.rb
+++ app/models/hscode_search.rb
@@ -1,11 +1,16 @@
 class HscodeSearch
 
-  def initialize(search_term)
-    @search_term = search_term
+  def initialize(search_terms)
+    @search_terms = search_terms
   end
 
   def search
-    search_result = Hscode.where("description ILIKE ? OR code::varchar(255) LIKE ?", "%#{@search_term}%", "%#{@search_term}%")
+    description_clause = @search_terms.split(' ').map{|x| "description ILIKE '%#{x}%'" }.join(' AND ')
+    code_clause = @search_terms.split(' ').map{|x| "code::varchar(255) LIKE '%#{x}%'" }.join(' AND ')
+
+    search_result = Hscode.where(description_clause + " OR " + code_clause)
     return search_result
   end
 end
+
+# Hscode.where("description ILIKE ? OR code::varchar(255) LIKE ?", "%#{@search_term}%", "%#{@search_term}%")
