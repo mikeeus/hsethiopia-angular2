96b626ea875cb062d1daf8a1528bb44e2b06f6ca
Mikias <itsmikias@gmail.com>
2016-08-07 10:46:25 +0300

    Fix hscode search service, change response.data() to response, limit search to > 3 characters

--- ang-client/src/search/hscode-search.component.ts
+++ ang-client/src/search/hscode-search.component.ts
@@ -1,19 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }            from '@angular/router';
 import { Observable }        from 'rxjs/Observable';
-// Observable class extensions
-import 'rxjs/add/observable/of';
-import 'rxjs/add/observable/throw';
-
-// Observable operators
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/switchMap';
-
 import { Subject }           from 'rxjs/Subject';
 import { HscodeSearchService } from './hscode-search.service';
 import { Hscode } from '../models/hscode';
--- ang-client/src/search/hscode-search.service.ts
+++ ang-client/src/search/hscode-search.service.ts
@@ -5,8 +5,10 @@ import {Hscode}           from '../models/hscode';
 export class HscodeSearchService {
   constructor(private http: Http) {}
   search(term: string) {
-    return this.http
-               .get(`http://localhost:3000/api/hscodes/search/${term}`)
-               .map((r: Response) => r.json().data as Hscode[]);
+    if (term.length > 3) {
+      return this.http
+                 .get(`http://localhost:3000/api/hscodes/search/${term}`)
+                 .map((r: Response) => r.json() as Hscode[]);
+    }
   }
 }
