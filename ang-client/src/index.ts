647426cc0b4bbb16f336d8b0f7930ac6061107c8
Mikias <itsmikias@gmail.com>
2016-08-08 10:21:29 +0300

    Add bootflat

--- ang-client/src/index.ts
+++ ang-client/src/index.ts
@@ -4,6 +4,7 @@ import 'es6-shim';
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import 'bootstrap/dist/css/bootstrap.css';
+import './vendor/bootflat/css/bootflat.min.css';
 import 'chart.js';
 import "./rxjs-extension";
 import {bootstrap} from '@angular/platform-browser-dynamic';
