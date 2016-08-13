5aef3bb36b869499b0b99a6accbdbc92e2830cba
Mikias <itsmikias@gmail.com>
2016-08-13 10:08:49 +0300

    Add gotoDashboard link on little button

--- ang-client/src/app/footer.ts
+++ ang-client/src/app/footer.ts
@@ -1,8 +1,15 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'Footer',
   templateUrl: './app/footer.html',
   styleUrls: ['./app/footer.scss']
 })
-export class Footer {}
+export class Footer {
+  constructor(private router: Router) {}
+
+  gotoDashboard() {
+    this.router.navigate(['dashboard']);
+  }
+}
